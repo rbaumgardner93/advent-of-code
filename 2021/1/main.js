1f64e3da79cb63c3efa9d79366f0f9bc37bbd124
Ryan Baumgardner <rbaumgardner@planview.com>
2021-12-01 16:28:07 -0500

    Complete day 1

---
+++ 2021/1/main.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'input.txt');
+
+const fileContent = fs.readFileSync(filePath, 'utf8');
+const data = fileContent.split("\n").map( m => +m );
+
+function getMeasurementIncreases( measurements ) {
+	let count = 0;
+
+	for( i = 0; i < measurements.length; i++ ) {
+		let prev = measurements[i - 1];
+		let curr = measurements[i];
+
+		if ( curr > prev ) {
+			count++;
+		}
+	}
+
+	return count;
+}
+
+function getSlidingWindowIncreases( measurements ) {
+	let count = 0;
+
+	let prev = measurements[0];
+	for(i = 3; i < measurements.length; i++) {
+		if (prev < measurements[i]) {
+			count++;
+		}
+
+		prev = measurements[i - 2]
+	}
+
+	return count;
+}
+
+console.log(`Sonar Sweep Part 1: ${getMeasurementIncreases(data)}`);
+console.log(`Sonar Sweep Part 2: ${getSlidingWindowIncreases(data)}`);
